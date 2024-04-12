e9b5dded2a6696e24e75fbdfd3d25d2ffb9f4507
Akira <junrongfu6@gmail.com>
2024-04-12 22:41:15 +0800

    commit

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-    <p>Product details for {{ id }}</p>
+    <p>{{ product.title }}</p>
+    <p>$ {{ product.price }}</p>
     <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim tempore
-      delectus vitae, nesciunt itaque harum aliquam minus incidunt similique
-      explicabo.
+      {{ product.description }}
     </p>
   </div>
 </template>
 
 <script setup>
 const { id } = useRoute().params;
+const uri = "https://fakestoreapi.com/products/" + id;
+
+//fetch the product
+const { data: product } = await useFetch(uri, { key: id });
 
 definePageMeta({
   layout: "products",
