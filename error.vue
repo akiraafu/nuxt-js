4fb15451b6ebb0152d491b67f8a889c5163c3dfd
Akira <junrongfu6@gmail.com>
2024-04-13 12:12:28 +0800

    commit

--- error.vue
+++ error.vue
@@ -3,11 +3,14 @@
     <p class="mt-7 text-7xl">{{ error.statusCode }}</p>
     <p class="mt-7 text-6xl">Ooops.</p>
     <p class="mt-7">{{ error.message }}</p>
+
+    <button class="btn m-7" @click="handleClearError">Go Home...</button>
   </div>
 </template>
 
 <script setup>
 defineProps(["error"]);
+const handleClearError = () => clearError({ redirect: "/" });
 </script>
 
 <style scoped></style>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -11,6 +11,13 @@ const uri = "https://fakestoreapi.com/products/" + id;
 //fetch the product
 const { data: product } = await useFetch(uri, { key: id });
 
+if (!product.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Product not found",
+  });
+}
+
 definePageMeta({
   layout: "products",
 });
